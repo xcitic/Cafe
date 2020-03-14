37e586ec2758313bc4d8780337da95014cf4409e
Sami <sami@netmaking.no>
2020-03-14 22:51:45 +0100

    easy setup script

--- setup.sh
+++ setup.sh
@@ -33,11 +33,8 @@ yarn run prod
 # setup database file sqlite
 touch database/database.sqlite
 php artisan migrate
-# Setup passport keys
-php artisan passport:install
 # Seed with dummy data
 php artisan db:seed
-
 # Dump autoload
 composer dump-autoload
 
